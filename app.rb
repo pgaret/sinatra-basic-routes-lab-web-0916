6128908948f5ab6242b034909fa92abebdaedfc0
pgaret <peregringaret@gmail.com>
2016-10-13 11:23:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Peregrin"
+  end
+  get '/hometown' do
+    "My hometown is San Marino"
+  end
+  get '/favorite-song' do
+    "My favorite song is Hysteria"
+  end
 end
